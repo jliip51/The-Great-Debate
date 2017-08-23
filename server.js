6c46b879a58b0d5dfe7f830234f64abf981dcdc7
David Thrasher <thrasher1332@hotmail.com>
2017-08-22 21:05:59 -0400

    Update Server.JS with Sequelize

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
+
+
 var express = require('express');
+var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+var handleBars = require('express-handlebars');
+var db = require("./models");
+var path = require('path');
+
 
 var app = express();
-
 var PORT = process.env.PORT || 8080;
 
+
+app.use(express.static(__dirname + "/public"));
+//BP
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.urlencoded({ extended: false}));
 
-require('./routes/apiRoutes')(app);
 
-app.listen(PORT, function(err){
-  if(err) throw err;
-  console.log(`App connected on PORT: ${PORT}`);
-})
+app.set('views', path.join(__dirname, 'views/'));
+app.use(methodOverride("_method"));
+app.engine("handlebars", handleBars({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+
+var routes = require("./controllers");
+app.use('/', routes);
+
+
+db.sequelize.sync({}).then(function() {
+	app.listen(PORT, function() {
+		console.log("Listening on port: " + PORT);
+	});
+});
