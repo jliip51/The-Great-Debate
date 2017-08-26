f463bcfcc92f2d231be7a0e878f4d1f78f2c5c00
jliip51 <jliip51@gmail.com>
2017-08-26 11:36:51 -0400

    more model changes

--- models/comments.js
+++ models/comments.js
@@ -3,8 +3,17 @@
 //Need to add attributes (i.e. AllowNull, Validate) to model definition.
 module.exports = function(sequelize, DataTypes) {
   var Comments = sequelize.define('Comments', {
-    body: DataTypes.TEXT,
-    votes: DataTypes.INTEGER,
+    body: {
+      allowNull: false,
+      type: DataTypes.TEXT
+      validate: {
+        len: [1]
+      }
+    }
+    votes: {
+      defaultValue: 0,
+      type: DataTypes.INTEGER
+    }
     links: DataTypes.TEXT
   });
     Comments.associate = function(models) {
