a33a627bf3216ff1b0a5503c179c23939332ae99
Siddharth Patel <siddharthpatel845@gmail.com>
2017-08-27 14:43:50 -0400

    added hooks and instance methods

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 'use strict';
+var bcrypt=require("bcrypt-nodejs");
 module.exports = function(sequelize, DataTypes) {
   var Users = sequelize.define('Users', {
     id: {
@@ -24,6 +25,13 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       type: DataTypes.STRING
     }
+  });
+  Users.prototype.validPassword=function(password) {
+    return bcrypt.compareSync(password, this.password);
+  }
+  Users.hook("beforeCreate",function(user,options,cb){
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    cb(null, options);
   });
       Users.associate = function(models) {
         Users.hasMany(models.Comments, {
