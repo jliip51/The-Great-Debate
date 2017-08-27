850223cd618a192eba56410f339995ce7473784a
jliip51 <jliip51@gmail.com>
2017-08-27 02:02:05 -0400

    added animated background etc.

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use('/add', routes);
 app.use('/login', routes);
 
 
-db.sequelize.sync({force: false}).then(function() {
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function(err) {
 		if (err) throw err;
 		console.log("Listening on port: " + PORT);
