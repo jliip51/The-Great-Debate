4c42d090413ee3ddd7ce0d72b8bd65cf671091e1
Siddharth Patel <siddharthpatel845@gmail.com>
2017-08-27 16:58:38 -0400

    login

--- public/js/app.js
+++ public/js/app.js
@@ -27,27 +27,14 @@ $(document).ready(function() {
       return;
     }
     
-    // If we have an email and password we run the loginUser function and clear the form
-    loginUser(userCreds.email, userCreds.password);
     $('#email').val("");
     $('#password').val("");
     
-    // console.log(userCreds);
-    // $.post("/signin", userCreds).then(function(data) {
-    //   console.log(data); //send to passport?//
-    // });
-  };
-  function loginUser(email, password) {
-    $.post("/api/login", {
-      email: email,
-      password: password
-    }).then(function(data) {
-      console.log(data);
-      // If there's an error, log the error
-    }).catch(function(err) {
-      console.log(err);
+    console.log(userCreds);
+    $.get("/signin", userCreds).then(function(data) {
+      console.log(data); //send to passport?//
     });
-  }
+  };
   function handleUserFormSignUp(event) {
     event.preventDefault();
     console.log('working');
