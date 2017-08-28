9069836be86234079474977a9be47d01266aa1ef
Siddharth Patel <siddharthpatel845@gmail.com>
2017-08-28 13:42:04 -0400

    passport added

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var bodyParser = require('body-parser');
 var handleBars = require('express-handlebars');
 var db = require("./models");
 var path = require('path');
-
+var session = require("express-session");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -18,6 +20,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+//sessions
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.set('views', path.join(__dirname, 'views/'));
 app.use(methodOverride("_method"));
 app.engine("handlebars", handleBars({ defaultLayout: "main"}));
@@ -25,10 +32,10 @@ app.set("view engine", "handlebars");
 
 var routes = require("./controllers/controller.js");
 app.use('/', routes);
-app.use('/add', routes);
-app.use('/signup', routes);
-app.use('/signin', routes);
-app.use('/about', routes);
+// app.use('/add', routes);
+//app.use('/signup', routes);
+//app.use('/signin', routes);
+// app.use('/about', routes);
 
 
 db.sequelize.sync({force: false}).then(function() {
