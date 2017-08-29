2e269cbf27ec46b5b1b743299dda251319b07492
Siddharth Patel <siddharthpatel845@gmail.com>
2017-08-28 21:26:38 -0400

    signup signin

--- server.js
+++ server.js
@@ -12,6 +12,42 @@ var LocalStrategy = require("passport-local").Strategy;
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var expressValidator = require("express-validator");
+var flash = require("connect-flash");
+
+//sessions
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+//Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
 
 app.use(express.static(__dirname + "/public"));
 //BP
@@ -19,11 +55,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-//sessions
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.set('views', path.join(__dirname, 'views/'));
 app.use(methodOverride("_method"));
