f8a936bab383b843b9623896d9546addc98c7aa3
jliip51 <jliip51@gmail.com>
2017-08-31 02:56:19 -0400

    some styling, cleaning, change vote count and prevent additional upvote in session.

--- public/js/comment.js
+++ public/js/comment.js
@@ -31,18 +31,17 @@ $(document).ready(function() {
 
   $('.upvote').on('click', function() {
     var voteCount = $(this).attr('data-count');
-    console.log('voteCount----')
-    console.log(voteCount)
-
+    var postid = $(this).attr('data-postid');
     var id = $(this).attr('data-id');
+    var newVoteCt = parseInt(voteCount) + 1
     var upvote = {
       id: parseInt(id),
-      votes: parseInt(voteCount) + 1,
+      votes: newVoteCt,
     }
-    console.log('jsupvote')
-    console.log(upvote)
     $.post("/upvote", upvote).then(function(data) {
-      return data;
+
+      $('#comment' + id).removeClass('upvote').toggleClass('btn-success btn-danger');
+      $('#upvote' + id).html(newVoteCt);
     }).catch(function(err) {
       console.log(err);
     });
