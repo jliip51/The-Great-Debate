952597aaee85ed55dd2d4d487b1397d59748207c
jliip51 <jliip51@gmail.com>
2017-08-31 15:34:55 -0400

    updated seed.sql

--- seeds.sql
+++ seeds.sql
@@ -1,4 +1,16 @@
 Use GR8DB8;
+DROP TABLE IF EXISTS `Comments`;
+DROP TABLE IF EXISTS `Posts`;
+DROP TABLE IF EXISTS `Users`;
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE IF NOT EXISTS `Users` (`id` CHAR(36) BINARY NOT NULL , `username` VARCHAR(255) NOT NULL UNIQUE, `email` VARCHAR(255) NOT NULL UNIQUE, `password` VARCHAR(255) NOT NULL, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, UNIQUE `Users_username_unique` (`username`), UNIQUE `Users_email_unique` (`email`), PRIMARY KEY (`id`)) ENGINE=InnoDB;
+SHOW INDEX FROM `Users` FROM `gr8db8`
+DROP TABLE IF EXISTS `Posts`;
+CREATE TABLE IF NOT EXISTS `Posts` (`id` INTEGER NOT NULL auto_increment , `category` VARCHAR(255) NOT NULL, `topic` VARCHAR(255) NOT NULL, `description` TEXT NOT NULL, `expired` TINYINT(1) DEFAULT false, `comment_count` INTEGER DEFAULT 0, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB;
+SHOW INDEX FROM `Posts` FROM `gr8db8`
+DROP TABLE IF EXISTS `Comments`;
+CREATE TABLE IF NOT EXISTS `Comments` (`id` INTEGER NOT NULL auto_increment , `body` TEXT NOT NULL, `votes` INTEGER DEFAULT 0, `links` TEXT, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, `UserId` CHAR(36) BINARY, `PostId` INTEGER, PRIMARY KEY (`id`), FOREIGN KEY (`UserId`) REFERENCES `Users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE, FOREIGN KEY (`PostId`) REFERENCES `Posts` (`id`) ON DELETE SET NULL ON UPDATE CASCADE) ENGINE=InnoDB;
+SHOW INDEX FROM `Comments` FROM `gr8db8`
 
 INSERT INTO posts (id, topic, description, category, start, expired, createdAt, updatedAt)
 VALUES ("1", "Its the end of the world, right?", "Armagettingready! or Amagettwhat?", "science", CURRENT_TIME(), false , CURRENT_TIME(), CURRENT_TIME());
@@ -20,16 +32,3 @@ INSERT INTO comments (body, createdAt, updatedAt, UserId, PostId)
 VALUES ("Superman is better than Batman", false, "Batman is just plain awesome", CURRENT_TIME(), CURRENT_TIME(), "54321" , "2");
 INSERT INTO comments (body, createdAt, updatedAt, UserId, PostId)
 VALUES ("Star Trek is Better than Star Wars", false, "What planet were you born on where Star Trek is better than Star Wars", CURRENT_TIME(), CURRENT_TIME(), "45234", "3");
-
-DROP TABLE IF EXISTS `Comments`;
-DROP TABLE IF EXISTS `Posts`;
-DROP TABLE IF EXISTS `Users`;
-DROP TABLE IF EXISTS `Users`;
-CREATE TABLE IF NOT EXISTS `Users` (`id` CHAR(36) BINARY NOT NULL , `username` VARCHAR(255) NOT NULL UNIQUE, `email` VARCHAR(255) NOT NULL UNIQUE, `password` VARCHAR(255) NOT NULL, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, UNIQUE `Users_username_unique` (`username`), UNIQUE `Users_email_unique` (`email`), PRIMARY KEY (`id`)) ENGINE=InnoDB;
-SHOW INDEX FROM `Users` FROM `gr8db8`
-DROP TABLE IF EXISTS `Posts`;
-CREATE TABLE IF NOT EXISTS `Posts` (`id` INTEGER NOT NULL auto_increment , `category` VARCHAR(255) NOT NULL, `topic` VARCHAR(255) NOT NULL, `description` TEXT NOT NULL, `expired` TINYINT(1) DEFAULT false, `comment_count` INTEGER DEFAULT 0, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB;
-SHOW INDEX FROM `Posts` FROM `gr8db8`
-DROP TABLE IF EXISTS `Comments`;
-CREATE TABLE IF NOT EXISTS `Comments` (`id` INTEGER NOT NULL auto_increment , `body` TEXT NOT NULL, `votes` INTEGER DEFAULT 0, `links` TEXT, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, `UserId` CHAR(36) BINARY, `PostId` INTEGER, PRIMARY KEY (`id`), FOREIGN KEY (`UserId`) REFERENCES `Users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE, FOREIGN KEY (`PostId`) REFERENCES `Posts` (`id`) ON DELETE SET NULL ON UPDATE CASCADE) ENGINE=InnoDB;
-SHOW INDEX FROM `Comments` FROM `gr8db8`
